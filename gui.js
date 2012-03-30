430f5711ba7174eeb304f40257a23094df36ce8d
Pablo Gonzalez Alonso <pabs87@gmail.com>
2012-03-30 16:26:13 +0100

    more dummy stuff, just before adding the read data

---
+++ gui.js
@@ -0,0 +1,31 @@
+
+String.format = function() {
+  var s = arguments[0];
+  for (var i = 0; i < arguments.length - 1; i++) {       
+    var reg = new RegExp("\\{" + i + "\\}", "gm");             
+    s = s.replace(reg, arguments[i + 1]);
+  }
+
+  return s;
+}
+
+var ITEM_TEMPLATE =" <li><a href='#'>{0}<span class='small right'>{1}</span><div class='small'>{2}<span class='right'>{3}km away</span></div></a></li>";
+var DESCRIPTION_LENGTH_LIMIT = 20;
+
+function createResultItem(oportunity) {
+	var title = oportunity.title;
+	var date = oportunity.date;
+	var description = oportunity.description;
+	if (description.length > DESCRIPTION_LENGTH_LIMIT) {
+		description = description.substring(0, DESCRIPTION_LENGTH_LIMIT - 3) + "...";
+	};
+	var distance = oportunity.distance;
+
+	return ITEM_TEMPLATE.format(title, date, description, distance);
+}
+
+function populateSearch(oportunities) {
+	for(var i = 0, n = oportunities.length; i < n;i++) {
+		
+	}
+}
